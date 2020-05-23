d648b6a18befd3a003660f170ddef839a503241a
Tomasz Ogrodniczuk <tomasz.ogrodniczuk@wp.pl>
2020-05-23 12:05:26 +0200

    ksiegozbior

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BookManager {
-
     private List<Book> books = new ArrayList<>();
 
     public Book createBook (String title, String author) {
