ca08bb07e3199d0846afd5ff232ccdc48310f1ab
Tomasz Ogrodniczuk <tomasz.ogrodniczuk@wp.pl>
2020-08-20 13:03:53 +0200

    Moja

--- moja/src/main/java/MojaTest.java
+++ moja/src/main/java/MojaTest.java
@@ -1,7 +1,7 @@
 public class MojaTest {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Start czekania");
-        Thread.sleep(10000);
+        Thread.sleep(5000);
         System.out.println("Koniec czekania");
     }
 }
